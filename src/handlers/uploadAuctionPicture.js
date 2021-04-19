ded8e334d3d050c2299c713c34a2dfc60c29346b
purisquiri <mau.dorigo@outlook.es>
2021-04-19 18:11:47 -0400

    uploading pictures complete lambda and dynamo conection

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,5 +1,9 @@
+import middy from "middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 import { getAuctionById } from "./getAuction";
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
@@ -7,13 +11,20 @@ export async function uploadAuctionPicture(event) {
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
 
-  const uploadToS3Result = await uploadPictureToS3(auction.id + ".jpg", buffer);
-  console.log(uploadToS3Result);
+  let updatedAuction;
+
+  try {
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+  } catch (e) {
+    console.error(e);
+    throw new createError.InternalServerError(e);
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
